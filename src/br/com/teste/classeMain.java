3a4d095e07501676cae2a2b2de91e2c6ce766f0b
Temistocles Zwang <temis2st@gmail.com>
2021-12-28 10:21:48 -0300

    testando IDE

---
+++ src/br/com/teste/classeMain.java
@@ -0,0 +1,10 @@
+package br.com.teste;
+
+import br.com.teste.model.Gato;
+
+public class classeMain {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Xanin");
+    }
+}
+
