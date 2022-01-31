b48a4b933d1cff26af274299b112d0589611cac0
Temistocles Zwang <temis2st@gmail.com>
2022-01-31 10:49:12 -0300

    removida pasta src

--- src/com/Exercicios/Metodos/Exercicio1/FormatadorDeSaida.java
+++
@@ -1,7 +0,0 @@
-package com.Exercicios.Metodos.Exercicio1;
-
-public class FormatadorDeSaida {
-    public static void formatarSaida(String mensagem){
-        System.out.println(mensagem);
-    }
-}
